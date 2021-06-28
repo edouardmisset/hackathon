f45f381e0efab51ee2149d703a8d3e25aa29fd4d
Cécile Dubois <duboiscecilepro@gmail.com>
2021-06-28 17:17:34 +0200

    Created route for new event

--- routes/newEvents.js
+++ routes/newEvents.js
@@ -1,13 +1,13 @@
-const eventRouter = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const newEvent = require('../models/newEvent');
+// const eventRouter = require('express').Router();
+// const asyncHandler = require('express-async-handler');
+// const newEvent = require('../models/newEvent');
 
-eventRouter.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    // const { titleOrContentContains, authorId } = req.query;
-    // res.send(await newEvent.findMany());
-  })
-);
+// eventRouter.get(
+//   '/',
+//   asyncHandler(async (req, res) => {
+// const { titleOrContentContains, authorId } = req.query;
+// res.send(await newEvent.findMany());
+//   })
+// );
 
-module.exports = eventRouter;
+// module.exports = eventRouter;
