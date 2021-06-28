bf76149fea9587e53babe4d3dd2915eb59b9af9b
Edouard Misset <edouardmisset@gmail.com>
2021-06-28 22:43:00 +0200

    Tried to fix the seed

--- models/event.js
+++ models/event.js
@@ -36,9 +36,37 @@ const create = ({
     },
   });
 
+const createMany = ({
+  numberOfEvents,
+  eventType,
+  ownerId,
+  name,
+  location,
+  image,
+  duration,
+  date,
+  description,
+  online,
+}) =>
+  db.event.createMany({
+    data: Array(numberOfEvents)
+      .fill(null)
+      .map(() => ({
+        eventType,
+        ownerId,
+        name,
+        location,
+        image,
+        duration,
+        date,
+        description,
+        online,
+      })),
+  });
+
 const findUnique = (id) =>
   db.event.findUnique({ where: { id: parseInt(id, 10) } });
 
 const destroy = (id) => db.event.delete({ where: { id: parseInt(id, 10) } });
 
-module.exports = { findMany, create, findAll, findUnique, destroy };
+module.exports = { findMany, create, createMany, findAll, findUnique, destroy };
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,21 +1,32 @@
 const faker = require('faker');
 const db = require('../db');
+const user = require('../models/user');
+const event = require('../models/event');
 
 module.exports = async function seed() {
-  await db.event.createMany({
-    data: Array(10)
-      .fill(null)
-      .map(() => ({
-        name: faker.name.firstName(),
-        description: faker.name.jobDescriptor(),
-        online: faker.datatype.boolean(),
-        date: faker.datatype.datetime(),
-        duration: faker.datatype.number(),
-        image: faker.image.image(),
-        location: faker.address.city(),
-      })),
+  await user.create({
+    data: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    },
+  });
+
+  await event.createMany({
+    numberOfEvents: 10,
+    eventType: 'Web Dev',
+    name: faker.name.firstName(),
+    description: faker.name.jobDescriptor(),
+    online: faker.datatype.boolean(),
+    date: faker.datatype.datetime(),
+    duration: faker.datatype.number(),
+    image: faker.image.image(),
+    location: faker.address.city(),
+    ownerId: 1,
   });
 };
+
 module
   .exports()
   .catch((e) => {
