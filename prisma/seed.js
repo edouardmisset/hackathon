286095df2cac71857081a818723be4279cdfc634
Edouard Misset <edouardmisset@gmail.com>
2021-06-29 23:21:00 +0200

    a better seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,29 +3,39 @@ const user = require('../models/user');
 // const event = require('../models/event');
 const db = require('../db');
 
+const eventNameList = [
+  'Focus group on Digital Marketing',
+  'Task force Web Assembly',
+  'Discussion on the evolution of digital sales',
+  'Drinks over best UX parctices',
+  'Client fidelity',
+  'Love or death of freelance work',
+  'Meeting : Career in Tech',
+  'Wednesday ruby breakfast',
+  'New Hype Event - SECRET',
+];
+
 console.log('test');
 module.exports = async function seed() {
   await user.create({
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    email: faker.internet.email(),
+    firstName: 'Franck',
+    lastName: 'Castle',
+    email: 'franck.castle@pun.ish',
     password: 'hello',
   });
 
   await db.event.createMany({
-    data: Array(10)
-      .fill(null)
-      .map(() => ({
-        name: faker.name.firstName(),
-        description: faker.name.jobDescriptor(),
-        online: faker.datatype.boolean(),
-        date: faker.datatype.datetime(),
-        duration: faker.datatype.number(),
-        image: faker.image.image(),
-        location: faker.address.city(),
-        popularity: parseInt(Math.random() * 1000, 10),
-        ownerId: 1,
-      })),
+    data: eventNameList.map((eventName) => ({
+      name: eventName,
+      description: faker.lorem.paragraph(),
+      online: faker.datatype.boolean(),
+      date: faker.date.soon(),
+      duration: parseInt((Math.random() + 30) * 8, 10),
+      image: faker.image.business(),
+      location: faker.address.city(),
+      popularity: parseInt(Math.random() * 1000, 10),
+      ownerId: 1,
+    })),
   });
 };
 
