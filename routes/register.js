77368e2c784dabb92f5e5a9663d6d8b207c30496
Edouard Misset <edouardmisset@gmail.com>
2021-06-29 14:36:38 +0200

    fixed integer

--- routes/register.js
+++ routes/register.js
@@ -5,7 +5,8 @@ const Register = require('../models/register');
 registerRoute.post(
   '/',
   asyncHandler(async (req, res) => {
-    const { eventId, userId } = req.body;
+    const eventId = parseInt(req.body.eventId, 10);
+    const userId = parseInt(req.body.userId, 10);
     try {
       res.status(200).send(await Register.create({ eventId, userId }));
     } catch (error) {
