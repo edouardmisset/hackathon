097e88889edf92e6ebffe223e32367e800c21551
JonathanGab <jogaborit@hotmail.com>
2021-06-29 15:55:14 +0200

    create sign up function

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const eventsRouter = require('./events');
 const newEventRouter = require('./newEvents');
 const profileRouter = require('./profiles');
 const skillRouter = require('./skills');
+const usersRouter = require('./users');
 
 module.exports = (app) => {
   app.use('/events', eventsRouter);
   app.use('/events/:id', eventsRouter);
+  app.use('/user', usersRouter);
   app.use('/new-event', newEventRouter);
   app.use('/profiles', profileRouter);
   app.use('/skills', skillRouter);
